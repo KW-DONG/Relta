4077372b4fe993379b0cc1dccfa6937f537914a7
KW-DONG <2464086773@qq.com>
2020-05-31 01:32:05 +0800

    531

---
+++ module/planner_new.c
@@ -0,0 +1,146 @@
+#include "type.h"
+#include "config.h"
+#include "planner_new.h"
+#include "delta.h"
+#include <math.h>
+#include "buffer.h"
+
+
+
+void Line_Z_Planner(float dz)
+{
+    uint32_t step = dz * STEPS_PER_UNIT;
+    
+    static block_t new_block;
+    uint8_t dir;
+    if (dz>0)   dir = carriage_UP;
+    else        dir = carriage_DOWN;
+
+    for (uint8_t i=0;i<3;i++)
+    {
+        new_block.step[i] = step;
+        new_block.dir[i] = dir;
+    }
+    Block_Buff_Write(&new_block,&block_buffer);
+}
+
+
+void Line_XY_Planner(float* xyz_c, float* xyz_t, float feedrate)
+{
+    static float abc_l[3];
+    static float abc[3];
+    static float xyz_v[3];
+    static float abc_v[3];
+    uint8_t xy_single = 2;
+    uint8_t xy_double = 2;
+
+    Velocity_Decouple(xyz_c,xyz_t,xyz_v,feedrate);
+    Inverse_Kinematics(xyz_c,abc_l);
+
+    if ((xyz_c[0]-xyz_t[0])==0.f&&(xyz_c[1]-xyz_t[1])!=0.f)         xy_single = 1;
+    else if ((xyz_c[0]-xyz_t[0])!=0.f&&(xyz_c[1]-xyz_t[1])==0.f)    xy_single = 0;
+    else if ((xyz_c[0]-xyz_t[0])==0.f&&(xyz_c[1]-xyz_t[1])==0.f)    return;
+    else if (fabsf(xyz_c[0]-xyz_t[0])>fabsf(xyz_c[1]-xyz_t[1]))     xy_double = 0;
+    else                                                            xy_double = 1;
+
+    if (xy_single!=2)
+    {
+        for (;xyz_t[xy_single]!=xyz_c[xy_single];xyz_c[xy_single] = xyz_c[xy_single] + 0.1f*fabsf(xyz_c[xy_single]-xyz_t[xy_single])*INV(xyz_c[xy_single]-xyz_xy_singley0]))
+        {
+            Inverse_Kinematics(xyz_c,abc);
+            Jacobian_Matrix(xyz_v,xyz_c,abc,abc_v);
+            block_t new_block;
+            for (uint8_t i=0;i<3;i++)
+            {
+                new_block.step[i] = (uint32_t)(fabsf(abc[i] - abc_l[i])*(float)STEPS_PER_UNIT);
+                new_block.maximum_velocity[i] = abc_v[i];
+                if (abc[i]>abc_l[i])    new_block.dir[i] = carriage_UP;
+                else                    new_block.dir[i] = carriage_DOWN;
+                abc_l[i] = abc[i];
+            }
+            Block_Buff_Write(&new_block, &block_buffer);
+        }
+    }else
+    {
+        for (;xyz_t[0]!=xyz_c[0]&&xyz_t[1]!=xyz_c[1];)
+        {
+            Inverse_Kinematics(xyz_c,abc);
+            Jacobian_Matrix(xyz_v,xyz_c,abc,abc_v);
+            block_t new_block;
+            xyz_c[xy_double] += 0.1f*fabsf(xyz_c[xy_double]-xyz_t[xy_double])*INV(xyz_c[xy_double]-xyz_t[xy_double]);
+            if (1)   xyz_c[!xy_double] += 0.1f*fabsf(xyz_c[!xy_double]-xyz_t[!xy_double])*INV(xyz_c[!xy_double]-xyz_t[!xy_double]);
+            ///B's method
+            
+            
+            
+            for (uint8_t i=0;i<3;i++)
+            {
+                new_block.step[i] = (uint32_t)(fabsf(abc[i] - abc_l[i])*(float)STEPS_PER_UNIT);
+                new_block.maximum_velocity[i] = abc_v[i];
+                if (abc[i]>abc_l[i])    new_block.dir[i] = carriage_UP;
+                else                    new_block.dir[i] = carriage_DOWN;
+                abc_l[i] = abc[i];
+            }
+            Block_Buff_Write(&new_block, &block_buffer);
+        }
+    }
+
+}
+
+
+void Arc_Planner(float* xyz_c, float* xyz_t, float radius, float feedrate)
+{
+    static float xy_p[2];
+    Get_Pivot(xyz_t,xyz_c,radius,xy_p);
+    //arc angle
+
+
+    //arc length
+
+    //line 
+}
+
+
+
+void Get_Pivot(float* xyz_t, float* xyz_c, float radius, float* xy_p)
+{
+    //find pivot with radius and Pythagoras theorem
+    float d_ct = sqrtf(SQ(xyz_c[0]-xyz_t[0])+SQ(xyz_c[1]-xyz_t[1]));
+    
+    //vector from current point to target point
+    float ij_ct[2] = {xyz_t[0] - xyz_c[0], xyz_t[1] - xyz_c[1]};
+
+    //unit vector of ij_ct
+    float ij_ct_u[2] = {ij_ct[0]*INV(d_ct), ij_ct[1]*INV(d_ct)};
+
+    //middle point between current point and target point
+    float xy_m[2] = {(xyz_t[0]-xyz_c[0])*INV(2.0f), (xyz_t[0]-xyz_c[0])*INV(2.0f)};
+
+    //unit vector from middle point to pivot
+    float ij_mp_u[2];
+    if (radius>0)
+    {
+        ij_mp_u[0] = ij_ct_u[1];
+        ij_mp_u[1] = - ij_ct_u[0];
+    }else
+    {
+        ij_mp_u[0] = - ij_ct_u[1];
+        ij_mp_u[1] = ij_ct_u[0];
+    }
+    //distance between middle point and pivot
+    float d_mp = sqrtf(SQ(d_ct*INV(2.0f))+SQ(radius));
+
+    //vector from middle point to pivot
+    float ij_mp[2] = {ij_mp_u[0]*d_mp, ij_mp_u[1]*d_mp};
+
+    //pivot coordinate
+    xy_p[0] = ij_ct[0]*INV(2.0f)+ij_mp[0];
+    xy_p[1] = ij_ct[1]*INV(2.0f)+ij_mp[1];
+}
+
+void Velocity_Decouple(float* xyz_c, float* xyz_t, float* xyz_v, float v_n)
+{
+    float len = INV(sqrtf(SQ(xyz_t[0]-xyz_c[0])+SQ(xyz_t[1]-xyz_c[1])+SQ(xyz_t[2]-xyz_c[2])));
+    for (uint8_t i=0;i<3;i++)   xyz_v[i] = fabsf(xyz_c[i]-xyz_t[i])*INV(len)*v_n;
+}
+
---
+++ module/planner_new.h
@@ -0,0 +1,17 @@
+#ifndef PLANNER_NEW_H
+#define PLANNER_NEW_H
+#include "type.h"
+
+extern block_buff_t block_buffer;
+extern machine_t machine;
+
+void Line_Z_Planner(float dz);
+
+void Line_XY_Planner(float* xy_c, float* xy_t, float feedrate);
+
+void Arc_Planner(float* xy_c, float* xy_t, float r, float feedrate);
+
+void Get_Pivot(float* xyz_t, float* xyz_c, float radius, float* xy_p);
+
+void Velocity_Decouple(float* xyz_c, float* xyz_t, float* xyz_v, float v_n);
+#endif
